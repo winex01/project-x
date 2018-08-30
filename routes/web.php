771fbc790f833db98340c2c442dcfad7a75a5e7a
winex01 <winnie131212592@gmail.com>
2018-08-30 21:52:10 +0800

    WIP: email verification using queue and jobs

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/verifyemail/{token}', 'Auth\RegisterController@verify');
+
+Route::get('/home', 'HomeController@index')->name('home');
