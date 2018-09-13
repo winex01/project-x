11505dd268b65d7f32cda04797b512b6a0879195
winex01 <winnie131212592@gmail.com>
2018-09-13 19:44:23 +0800

    fix register form and convert to horizontal form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
         <div class="panel-heading">{{ __('Login') }}</div>
 
         <div class="panel-body">
-            <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
+            <form class="form-horizontal" method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
                 @csrf
 
-                <div class="form-group row {{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                <div class="form-group {{ $errors->has('name') ? ' has-error' : '' }}">
+                    <label for="name" class="control-label col-md-4">{{ __('Name') }}</label>
 
                     <div class="col-md-6">
                         <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' has-error' : '' }}" name="name" value="{{ old('name') }}" autofocus required>
@@ -23,7 +23,7 @@
                 </div>
 
                 <div class="form-group row {{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <label for="email" class="control-label col-md-4">{{ __('E-Mail Address') }}</label>
 
                     <div class="col-md-6">
                         <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' has-error' : '' }}" name="email" value="{{ old('email') }}" required>
@@ -37,7 +37,7 @@
                 </div>
 
                 <div class="form-group row {{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                    <label for="password" class="control-label col-md-4">{{ __('Password') }}</label>
 
                     <div class="col-md-6">
                         <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' has-error' : '' }}" name="password" required>
@@ -51,7 +51,7 @@
                 </div>
 
                 <div class="form-group row  {{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                    <label for="password-confirm" class="control-label col-md-4">{{ __('Confirm Password') }}</label>
 
                     <div class="col-md-6">
                         <input id="password-confirm" type="password" class="form-control {{ $errors->has('password') ? ' has-error' : '' }}" name="password_confirmation" required>
