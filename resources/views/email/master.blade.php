dd1ab6bdb52368f2438c657b790822674b93a047
winex01 <winnie131212592@gmail.com>
2018-09-15 23:48:15 +0800

    WIP template

--- resources/views/email/master.blade.php
+++ resources/views/email/master.blade.php
@@ -1,20 +1,4 @@
- <!DOCTYPE html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
-
-
-  <title>{{ env('APP_NAME') }}</title>
-
-  @include('layouts.header_scripts')
-
-</head>
-
+@include('layouts.header')
 <body class="hold-transition skin-blue layout-top-nav">
 <div class="wrapper">
 
