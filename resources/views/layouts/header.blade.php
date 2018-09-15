1c6d58fb5704dcb550959cdf674045de14fe0cd4
winex01 <winnie131212592@gmail.com>
2018-09-15 23:02:07 +0800

    WIP: refactor code and remove redandunt code

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+
+  <title>{{ env('APP_NAME') }}</title>
+
+  {{-- <link rel="icon" href="http://ebook.test/uploads/book-icon.png"> --}}
+
+  @include('layouts.header_scripts')
+
+</head>
+{{-- 
+BODY TAG OPTIONS:
+=================
+Apply one or more of the following classes to get the
+desired effect
+|---------------------------------------------------------|
+| SKINS         | skin-blue                               |
+|               | skin-black                              |
+|               | skin-purple                             |
+|               | skin-yellow                             |
+|               | skin-red                                |
+|               | skin-green                              |
+|---------------------------------------------------------|
+|LAYOUT OPTIONS | fixed                                   |
+|               | layout-boxed                            |
+|               | layout-top-nav                          |
+|               | sidebar-collapse                        |
+|               | sidebar-mini                            |
+|---------------------------------------------------------|
+ --}}
