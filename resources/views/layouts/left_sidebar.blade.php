19a78f1a823cd262f186dd4bb4817381d5dfef52
winex01 <winnie131212592@gmail.com>
2018-09-16 14:29:35 +0800

    WIP: tempalte

--- resources/views/layouts/left_sidebar.blade.php
+++ resources/views/layouts/left_sidebar.blade.php
@@ -2,16 +2,27 @@
 <ul class="sidebar-menu" data-widget="tree">
   <li class="header">{{ __('NAVIGATION') }}</li>
   <!-- Optionally, you can add icons to the links -->
-  <li class="treeview active">
+  
+  {{-- sample module --}}
+  {{-- <li class="treeview active">
     <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
       <span class="pull-right-container">
           <i class="fa fa-angle-left pull-right"></i>
         </span>
     </a>
+    sample tab
     <ul class="treeview-menu">
-      <li><a href="#">Link in level 2</a></li>
-      <li><a href="#">Link in level 2</a></li>
+      <li class=""><a href="#"><i class="fa fa-circle-o text-aqua">  </i> Book Lists</a>
+      </li>
+      <li class="active"><a href="#"><i class="fa fa-circle-o text-blue">  </i> Views</a>
+      </li>
+      <li class=""><a href="#"><i class="fa fa-circle-o text-green">  </i> Bookmarks</a>
+      </li>
+      <li class=""><a href="#"><i class="fa fa-circle-o text-red">  </i> Downloads</a>
+      </li>
     </ul>
-  </li>
+    end sample tab
+  </li> --}}
+  {{-- end sample module --}}
 </ul>
 <!-- /.sidebar-menu
